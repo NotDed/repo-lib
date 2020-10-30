d524bfb39d8bfd999976311dd79443a3f3671d43
Daniel <daniel.serna.olarte@gmail.com>
2020-10-29 22:10:01 -0500

    Creando modelo

---
+++ libreria/Modulos/Libreria/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django import forms
+
+# Create your models here.
+class root (models.Model):
+    idRoot = models.AutoField(primary_key=True)
+    password = models.CharField(max_length=100)
+
+class Usuario (models.Model):
+    DNI = models.AutoField(primary_key=True)
+    tipoUser = models.IntegerField()
+    password = models.CharField(max_length=100)
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    fechaDeNacimiento = models.DateField()
+    lugarDeNacimiento = models.CharField(max_length=100)
+    genero = models.CharField(max_length=100)
+    correoElectronico = models.CharField(max_length=100)
+    temasLiterarios = models.CharField(max_length=100)
+    noticias = models.BooleanField()
+
+class TarjetasDeCredito (models.Model):
+    idTarjeta = models.AutoField(primary_key=True)
+    tipo = models.IntegerField()
+    numeroTarjeta = models.CharField(max_length=100)
+    saldo = models.FloatField()
+    DNI = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
+
+class Autor (models.Model):
+    idAutor = models.AutoField(primary_key=True)
+    nombreAutor = models.CharField(max_length=100)
+
+
+class Libro (models.Model):
+    ISSN = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100)
+    genero = models.CharField(max_length=100)
+    numeroPaginas = models.IntegerField()
+    editorial = models.CharField(max_length=100)
+    idAutor = models.ForeignKey(Autor, null=False, blank=False, on_delete=models.CASCADE)
+
+
+class Compra (models.Model):
+    idCompra = models.AutoField(primary_key=True)
+    fechaDeCompra = models.DateTimeField()
+    DNI = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
+
+
+class Reseva (models.Model):
+    idReserva = models.AutoField(primary_key=True)
+    fechaDeReserva = models.DateTimeField()
+    DNI = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
+
+
+class Ejemplar (models.Model):
+    idEjemplar = models.AutoField(primary_key=True)
+    estado = models.IntegerField()
+    ISSN = models.ForeignKey(Libro, null=False, blank=False, on_delete=models.CASCADE)
+    idCompra = models.ForeignKey(Compra, null=True)
+    idReserva = models.ForeignKey(Reserva, null=True)
+
+
+class Noticia (models.Model):
+    idNoticia = models.AutoField(primary_key=True)
+    fechaPublicacion = models.DateTimeField()
+    descripcion = models.TextField()
+    ISSN = models.ForeignKey(Libro, null=False, blank=False, on_delete=models.CASCADE)
+
+
+class retirados (models.Model):
+    idRetirado = models.AutoField(primary_key=True)
+    fechaDeRetiro = models.DateTimeField()
+    ISSN = models.ForeignKey(Libro, null=False, blank=False, on_delete=models.CASCADE)
--- libreria/libreria/settings.py
+++ libreria/libreria/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
 TIME_ZONE = 'UTC'
 
@@ -111,7 +111,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
