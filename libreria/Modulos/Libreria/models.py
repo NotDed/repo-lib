b5a13d3dd29c1fd3f1c256ec7854fc7da7ae1cb8
Daniel <daniel.serna.olarte@gmail.com>
2020-10-29 22:17:34 -0500

    Ajustando Modelo

--- libreria/Modulos/Libreria/models.py
+++ libreria/Modulos/Libreria/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django import forms
 
 # Create your models here.
-class root (models.Model):
+class root(models.Model):
     idRoot = models.AutoField(primary_key=True)
     password = models.CharField(max_length=100)
 
-class Usuario (models.Model):
+
+class Usuario(models.Model):
     DNI = models.AutoField(primary_key=True)
     tipoUser = models.IntegerField()
     password = models.CharField(max_length=100)
@@ -19,19 +19,28 @@ class Usuario (models.Model):
     temasLiterarios = models.CharField(max_length=100)
     noticias = models.BooleanField()
 
-class TarjetasDeCredito (models.Model):
+    def getNombreCompleto(self):
+        txt = '{1}, {2}'
+        return txt.format(self.nombres, self.apellidos)
+
+
+class TarjetasDeCredito(models.Model):
     idTarjeta = models.AutoField(primary_key=True)
     tipo = models.IntegerField()
     numeroTarjeta = models.CharField(max_length=100)
     saldo = models.FloatField()
     DNI = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
 
-class Autor (models.Model):
+    def getSaldo(self):
+        return self.saldo
+
+
+class Autor(models.Model):
     idAutor = models.AutoField(primary_key=True)
     nombreAutor = models.CharField(max_length=100)
 
 
-class Libro (models.Model):
+class Libro(models.Model):
     ISSN = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=100)
     genero = models.CharField(max_length=100)
@@ -40,19 +49,19 @@ class Libro (models.Model):
     idAutor = models.ForeignKey(Autor, null=False, blank=False, on_delete=models.CASCADE)
 
 
-class Compra (models.Model):
+class Compra(models.Model):
     idCompra = models.AutoField(primary_key=True)
-    fechaDeCompra = models.DateTimeField()
+    fechaDeCompra = models.DateTimeField(auto_now_add=True)
     DNI = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
 
 
-class Reseva (models.Model):
+class Reseva(models.Model):
     idReserva = models.AutoField(primary_key=True)
-    fechaDeReserva = models.DateTimeField()
+    fechaDeReserva = models.DateTimeField(auto_now_add=True)
     DNI = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
 
 
-class Ejemplar (models.Model):
+class Ejemplar(models.Model):
     idEjemplar = models.AutoField(primary_key=True)
     estado = models.IntegerField()
     ISSN = models.ForeignKey(Libro, null=False, blank=False, on_delete=models.CASCADE)
@@ -60,14 +69,14 @@ class Ejemplar (models.Model):
     idReserva = models.ForeignKey(Reserva, null=True)
 
 
-class Noticia (models.Model):
+class Noticia(models.Model):
     idNoticia = models.AutoField(primary_key=True)
-    fechaPublicacion = models.DateTimeField()
+    fechaPublicacion = models.DateTimeField(auto_now_add=True)
     descripcion = models.TextField()
     ISSN = models.ForeignKey(Libro, null=False, blank=False, on_delete=models.CASCADE)
 
 
-class retirados (models.Model):
+class retirados(models.Model):
     idRetirado = models.AutoField(primary_key=True)
-    fechaDeRetiro = models.DateTimeField()
+    fechaDeRetiro = models.DateTimeField(auto_now_add=True)
     ISSN = models.ForeignKey(Libro, null=False, blank=False, on_delete=models.CASCADE)
